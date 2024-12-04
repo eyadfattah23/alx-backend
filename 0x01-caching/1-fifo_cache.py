d28d1118db8370e4ba45844bae88af0bdb3f93b1
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-12-04 16:15:08 +0200

    Create a class FIFOCache that inherits from BaseCaching and is a caching system

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Define a class FIFOCache that inherits from BaseCaching(caching system)"""
+
+
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class FIFOCache(BaseCaching):
+    """FIFO Based caching system
+    """
+
+    def __init__(self):
+        """INITIALIZE"""
+        super().__init__()
+
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data"""
+        if key and item:
+            self.cache_data[key] = item
+            keys = list(self.cache_data.keys())
+            if len(keys) > BaseCaching.MAX_ITEMS:
+                del self.cache_data[keys[0]]
+                print("DISCARD:", keys[0])
+
+    def get(self, key):
+        """return the value in self.cache_data linked to key"""
+
+        if key:
+            return self.cache_data.get(key)
